09896d3e596ba7efaa0503469cb8cf816c27065e
kaideas <kaitakahashi@Kais-MacBook-Pro-Retina-2.local>
2015-04-12 18:17:10 -0400

    Revert 3dc3d8b..a71be3e
    This rolls back to commit 3dc3d8b69ef2066efe20f9004b2dabcce1b5b557.

--- 20+80/js/script.js
+++ 20+80/js/script.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
 
-if (window.matchMedia('(max-width: 520px)').matches) {
+// var mq= window.matchMedia('@media screen and (max-width: 520px)');
+
+if($(window).width()<520) {
 
 		$("div.csmlogo").html('<img src="csmlogo2.svg">');
 		 console.log("yay");
