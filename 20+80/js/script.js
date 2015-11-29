4c291b303f9881427e915b76417723bf23766be3
kaideas <kaitakahashi@Kais-MacBook-Pro-Retina.local>
2015-11-28 20:39:15 -0500

    update

--- 20+80/js/script.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-
-if (window.matchMedia('(max-width: 520px)').matches) {
-
-		$("div.csmlogo").html('<img src="csmlogo2.svg">');
-		 console.log("yay");
-	}
-
-});
--- 20+80/script.js
+++
@@ -1,6 +0,0 @@
-function pageScroll() {
-    window.scrollBy(0,1);
-    scrolldelay = setTimeout(pageScroll(),10);
-
-    console.log("Hi");
-}
